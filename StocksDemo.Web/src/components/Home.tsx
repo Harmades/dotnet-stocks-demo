f9388ad979e31d6905715c8652637019eb2661d2
Adrian Lissot <adrian.lissot@gmail.com>
2025-03-21 21:38:13 +0000

    Resizable panel for details

--- StocksDemo.Web/src/components/Home.tsx
+++ StocksDemo.Web/src/components/Home.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { StocksDemoApiClient, Stock } from "../clients/StocksDemoApiClient";
 import { StocksComponent } from "./Stocks";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { getTheme, Separator } from "@fluentui/react";
 
 declare let stocksDemoConfig: any;
 
 export const Home: React.FC = () => {
+  const theme = getTheme();
   return (
-    <main>
-      <div id="app">
-        <StocksComponent />
-      </div>
-    </main>
+    <>
+      <PanelGroup direction="vertical">
+        <Panel>
+          <div style={{ overflow: "auto", height: "100%" }}>
+            <StocksComponent />
+          </div>
+        </Panel>
+        <PanelResizeHandle style={{ backgroundColor: theme.palette.accent, height: '4px'  }} >
+        </PanelResizeHandle>
+        <Panel>
+          bottom
+        </Panel>
+      </PanelGroup>
+    </>
   );
 }
